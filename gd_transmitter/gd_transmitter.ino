f19938f281a7e83ad63eb56cec5f9310d6d8501a
Suraj Joseph <surajj1234@gmail.com>
2016-09-28 13:32:24 -0400

    Added gd transmitter arduino pin interrupt setups

--- gd_transmitter/gd_transmitter.ino
+++ gd_transmitter/gd_transmitter.ino
@@ -1,41 +1,46 @@
+#include <util/delay.h>
+
 #define SetPin(Port, Bit)    (Port |= (1 << Bit))
 #define ClearPin(Port, Bit)  (Port &= ~(1 << Bit))
 
-#define GetPin(Port, Bit)    (Port & (1 << Bit))
-#define isHigh(Port, Bit)    (Port & (1 << Bit))
+#define isHigh(Port, Bit)    ((Port >> (Bit)) & 0x01)
 #define isLow(Port, Bit)     (!isHigh(Port, Bit))
 
 // Arduino Mega 2560 definitions
 static const int chip_select_pin = 2;   // PE4
-#define CHIP_SELECT_PORT    PORTE
-#define CHIP_SELECT_PIN     4
+#define CHIP_SELECT_OUT_PORT    PORTE
+#define CHIP_SELECT_IN_PORT     PINE
+#define CHIP_SELECT_PIN         4
 
 static const int clock_pin = 3;         // PE5
-#define CLOCK_PORT          PORTE
+#define CLOCK_OUT_PORT          PORTE
+#define CLOCK_IN_PORT           PINE
 #define CLOCK_PIN           5
 
 static const int data_pin = 4;          // PG5
-#define DATA_PORT           PORTG
+#define DATA_OUT_PORT           PORTG
+#define DATA_IN_PORT            PING
 #define DATA_PIN            5
 
 static const int tx_pin = 5;            // PE3 
-#define TX_PORT             PORTE
+#define TX_OUT_PORT             PORTE
+#define TX_IN_PORT              PINE
 #define TX_PIN              3
 
 
 static const uint8_t SOF_BYTE = 0xFE;
 static const uint8_t PKT_LEN = 0x08;
 
-static const uint8_t SNIFF_START = 0xDE;
-static const uint8_t SNIFF_STOP = 0xAD;
+static const uint8_t SNIFF_START = '#';
+static const uint8_t SNIFF_STOP = '\r';
 
 static uint8_t rx_buffer[PKT_LEN] = {0};
 static uint8_t rx_len = 0, pkt_len = 0, rx_byte = 0;
 
 static volatile bool captureData;
-static uint16_t captureLen = 0;
+static volatile uint16_t captureLen = 0;
 static uint8_t captureBuf[1000] = {0};
-static unsigned long last_sniff_us;
+static volatile unsigned long last_sniff_ms;
 
 typedef enum RxState
 {
@@ -56,32 +61,46 @@ void clk_ISR();
 
 void setup()
 {
+    Serial.begin(115200);
+    Serial.println();
+
     pinMode(chip_select_pin, INPUT);
     pinMode(clock_pin, INPUT);
     pinMode(data_pin, INPUT);
     pinMode(tx_pin, OUTPUT);
 
-    attachInterrupt(0, cs_ISR, CHANGE);
-    attachInterrupt(1, clk_ISR, RISING);
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
 
-    Serial.begin(115200);
-    Serial.println();
+    // disable timer0 overflow interrupt (millis/micros/delay wont work)
+    TIMSK0 &= ~_BV(TOIE0);
+
+    EICRB |= 1 << ISC40;  // sense any change on the INT0 pin
+    EIMSK |= 1 << INT4;   // enable INT0 interrupt
+
+    EICRB |= (1 << ISC51) | (1 << ISC50);  // sense rising edge on the INT1 pin
+    EIMSK |= 1 << INT5;   // enable INT1 interrupt
+
+    //attachInterrupt(0, cs_ISR, CHANGE);
+    //attachInterrupt(1, clk_ISR, RISING);
 
     rx_state = RX_SOF;
     captureData = false;
     captureLen = 0;
-    last_sniff_us = micros();
+    //last_sniff_ms = millis();
 }
 
 void loop()
 {
     sniff_eeprom_packets();
-    run_rx_state_machine();
+    //run_rx_state_machine();
 }
 
-void cs_ISR()
+//void cs_ISR()
+ISR(INT4_vect)
 {
-    if (isHigh(CHIP_SELECT_PORT, CHIP_SELECT_PIN))
+    int cs_pin = isHigh(CHIP_SELECT_IN_PORT, CHIP_SELECT_PIN);
+    if (cs_pin)
     {
         captureData = true;
         captureBuf[captureLen++] = SNIFF_START;
@@ -90,23 +109,34 @@ void cs_ISR()
     {
         captureData = false;
         captureBuf[captureLen++] = SNIFF_STOP;
+        captureBuf[captureLen++] = '\n';
     }
-    last_sniff_us = micros();
 }
 
-void clk_ISR()
+//void clk_ISR()
+ISR(INT5_vect)
 {
+    int data_pin = isHigh(DATA_IN_PORT, DATA_PIN);
     if (captureData)
     {
-        captureBuf[captureLen++] = GetPin(DATA_PORT, DATA_PIN);
+        if (data_pin)
+        {
+            captureBuf[captureLen++] = '1';
+        }
+        else
+        {
+            captureBuf[captureLen++] = '0';
+        }
     }
 }
 
 void sniff_eeprom_packets()
 {
+    _delay_ms(5000);
     if (captureLen > 0)
     {
-        if (micros() - last_sniff_us > 10000)
+
+        //if (millis() - last_sniff_ms > 5000)
         {
             Serial.write(captureBuf, captureLen);
             captureLen = 0;
